4e5dd8c556f2d69e5036e2c85fdc9766cc7133c9
Raptorshere <Kevinfula2@gmail.com>
2023-10-13 14:37:54 -0600

    changes for the array list 1 and 2 examples

--- client.js
+++ client.js
@@ -1,3 +1,47 @@
-//enter variables with an array list
-//change objects
-//make sure all functions and array lists  
+//enter properties and objects with an array list
+//add objects in the array
+//make sure all functions and array display the methods used
+
+
+// first array method with objects
+const house = {
+  door: 'handle',
+  bed: 'sleep',
+ staircase: 'climb',
+ television: 'entertain',
+  mirror: 'reflection'
+};
+
+
+// this is where I will log the array variable
+console.log(house);
+
+
+// for the second array method
+const realtor = {
+  name: 'Arthur',
+  age: '45',
+  field: 'housing',
+}
+
+console.log(realtor);
+
+
+if (hungry) {
+  console.log('Time for lunch!');
+}
+
+// adding the true or false statement for realtor example
+const highPriceCondo= false
+if (highPriceCondo)
+
+
+// second array method used 
+const burger = {
+  'bun', 'lettuce', 'meat', )
+
+// object method on hot dogs
+  const hotDog:  {ingredient:"Smokie", condiment:"ketchup", :46}; 
+
+  typeof: "ketchup"
+
